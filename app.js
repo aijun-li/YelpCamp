d7b65bd6aa7a281f1fccee4ba51a87a5ae3dcbde
Aijun Li <liaijun.cn@gmail.com>
2020-01-20 22:50:51 +0000

    Clean up some code and make the project more modular

--- app.js
+++ app.js
@@ -1,32 +1,15 @@
 var express = require('express')
     , app = express()
     , bodyParser = require('body-parser')
-    , mongoose = require('mongoose')
+    , mongoose = require('mongoose'),
+    Campground = require('./models/campground'),
+    seedDB = require('./seeds')
+
+seedDB()
 
 mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true, useUnifiedTopology: true })
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
-
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-})
-
-var Campground = mongoose.model('Campground', campgroundSchema)
-
-// Campground.create({
-//     name: 'Granite Hill', 
-//     image: 'https://photosforclass.com/download/pixabay-4522970?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2F52e5d7414355ac14f6da8c7dda793f7f1636dfe2564c704c722678d5954ac751_960.jpg&user=Ben_Frieden',
-//     description: 'This is a huge granite hill, no bathrooms. No water. Beautiful granite!'
-// }, function(err, campground) {
-//     if (err ){
-//         console.log(err)
-//     } else {
-//         console.log('NEWLY CREATED CAMPGROUND: ')
-//         console.log(campground)
-//     }
-// })
 
 app.get('/', function (req, res) {
     res.render('landing')
@@ -67,7 +50,7 @@ app.get('/campgrounds/new', function (req, res) {
 // SHOW - shows more info about one campground
 app.get('/campgrounds/:id', function (req, res) {
     // find the campground with provided ID
-    Campground.findById(req.params.id, function (err, foundCampground) {
+    Campground.findById(req.params.id).populate('comments').exec(function (err, foundCampground) {
         if (err) {
             console.log(err)
         } else {
