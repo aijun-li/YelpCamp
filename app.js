e8d60b7e49acce74a3e49c2258184b5d365458f1
Aijun Li <liaijun.cn@gmail.com>
2020-01-21 00:12:30 +0000

    Add basic comment function and optimize some code

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express')
     , bodyParser = require('body-parser')
     , mongoose = require('mongoose'),
     Campground = require('./models/campground'),
-    seedDB = require('./seeds')
+    seedDB = require('./seeds'),
+    Comment = require('./models/comment')
 
 seedDB()
 
@@ -21,7 +22,7 @@ app.get('/campgrounds', function (req, res) {
         if (err) {
             console.log(err)
         } else {
-            res.render('index', { campgrounds: allcampgrounds })
+            res.render('campgrounds/index', { campgrounds: allcampgrounds })
         }
     })
     // 
@@ -29,11 +30,7 @@ app.get('/campgrounds', function (req, res) {
 
 // CREATE - add new campgrounds
 app.post('/campgrounds', function (req, res) {
-    var name = req.body.name
-    var image = req.body.image
-    var description = req.body.description
-    var newCampground = { name: name, image: image, description: description }
-    Campground.create(newCampground, function (err, newlyCreated) {
+    Campground.create(req.body.campground, function (err, newlyCreated) {
         if (err) {
             console.log(err)
         } else {
@@ -44,7 +41,7 @@ app.post('/campgrounds', function (req, res) {
 
 // NEW - show form to create new campground
 app.get('/campgrounds/new', function (req, res) {
-    res.render('new')
+    res.render('campgrounds/new')
 })
 
 // SHOW - shows more info about one campground
@@ -55,10 +52,44 @@ app.get('/campgrounds/:id', function (req, res) {
             console.log(err)
         } else {
             // render show template with that campground
-            res.render('show', { campground: foundCampground })
+            res.render('campgrounds/show', { campground: foundCampground })
         }
     })
 
+})
+
+// ========================================
+// COMMENTS ROUTES
+// ========================================
+
+app.get('/campgrounds/:id/comments/new', function (req, res) {
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render('comments/new', { campground: campground })
+        }
+    })
+})
+
+app.post('/campgrounds/:id/comments', function (req, res) {
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) {
+            console.log(err)
+            res.redirect('/camgrounds')
+        } else {
+            Comment.create(req.body.comment, function (err, comment) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    campground.comments.push(comment)
+                    campground.save()
+                    res.redirect('/campgrounds/' + campground._id)
+                }
+            })
+
+        }
+    })
 })
 
 app.listen(2333, function () {
